5a97f4d2d0c53a478d0b24bde0c1b05ca79d04ed
Ruben De Haro Sánchez <rubensanchezdh@gmail.com>
2023-10-16 09:14:32 +0200

    Actividad_5 En Proceso

--- app/src/main/java/com/example/ejercicios_clase/MainActivity.kt
+++ app/src/main/java/com/example/ejercicios_clase/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.ejercicios_clase
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,35 +14,40 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ejercicios_clase.ui.theme.Ejercicios_ClaseTheme
 
-class MainActivity : ComponentActivity() {
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Ejercicios_ClaseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Se llama después de onCreate. Santi, muestro la pantalla. El usuario aún no puede interaccionar")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Ejercicios_ClaseTheme {
-        Greeting("Android")
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "El usuario ya puede interaccionar con la pantalla")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Pierdo el foco de la pantalla. Boton home")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Vuelvo a estar visible para santi.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Dejo de estar visible para el usuario. Otra App, S.O.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "La actividad muere.")
     }
 }
