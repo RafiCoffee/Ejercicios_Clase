0e15439509ab54a47ad24176ed6f4dd36cbd94d9
RafiCoffee <rubensanchezdh@hotmail.com>
2023-10-19 02:19:35 +0200

    Actividades Terminadas

--- app/src/main/java/com/example/ejercicios_clase/MainActivity.kt
+++ app/src/main/java/com/example/ejercicios_clase/MainActivity.kt
@@ -3,57 +3,53 @@ package com.example.ejercicios_clase
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ejercicios_clase.ui.theme.Ejercicios_ClaseTheme
 
-private var a: A = A("Ruben", "Rafi", 20, true)
+private var x: Int = 1
+private var y: Int = 2
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        onStart()
+        Log.d(TAG, "X: '$x' Y: '$y'" )
     }
-
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "Se llama después de onCreate. Santi, muestro la pantalla. El usuario aún no puede interaccionar")
-        a = A("Ruben", "Rafi", 20, true)
-        onResume()
+        x++; y++
+        Log.d(TAG, "X: '$x' Y: '$y'" )
+        val a = A("Ruben", "Rafi", 20, true)
+        Toast.makeText(this, a.toString(), Toast.LENGTH_LONG).show()
     }
-
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "El usuario ya puede interaccionar con la pantalla")
-        Toast.makeText(this, a.toString(), Toast.LENGTH_LONG).show()
+        x++; y--
+        Log.d(TAG, "X: '$x' Y: '$y'" )
     }
-
-    /*override fun onPause() {
+    override fun onPause() {
         super.onPause()
         Log.d(TAG, "Pierdo el foco de la pantalla. Boton home")
+        x--; y--
+        Log.d(TAG, "X: '$x' Y: '$y'" )
     }
-
     override fun onRestart() {
         super.onRestart()
         Log.d(TAG, "Vuelvo a estar visible para santi.")
+        x--; y++
+        Log.d(TAG, "X: '$x' Y: '$y'" )
     }
-
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "Dejo de estar visible para el usuario. Otra App, S.O.")
+        x--; y++
+        Log.d(TAG, "X: '$x' Y: '$y'" )
     }
-
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "La actividad muere.")
-    }*/
+        x++; y++
+        Log.d(TAG, "X: '$x' Y: '$y'" )
+    }
 }
--- app/src/main/java/com/example/ejercicios_clase/SecondActivity.kt
+++ app/src/main/java/com/example/ejercicios_clase/SecondActivity.kt
@@ -1,25 +1,85 @@
 package com.example.ejercicios_clase
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-private lateinit var boton : Button
-private lateinit var texto : TextView
-class SecondActivity : AppCompatActivity() {
+private lateinit var saludoBt: Button
+private lateinit var noSaludoBt: Button
+private lateinit var saludoClaseBt: Button
+class SecondActivity : AppCompatActivity()/*, View.OnClickListener*/ {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        boton = findViewById(R.id.PruebaSaludo)
-        texto = findViewById(R.id.Prueba)
+
+        saludoBt = findViewById(R.id.Saludo)
+        noSaludoBt = findViewById(R.id.NoSaludo)
+        saludoClaseBt = findViewById(R.id.SaludoClase)
+
+        saludoBt.setOnClickListener{
+            Toast.makeText(this, "Hola usuario", Toast.LENGTH_LONG).show()
+        }
+
+        noSaludoBt.setOnClickListener{
+            Toast.makeText(this, "No te saludo", Toast.LENGTH_LONG).show()
+        }
+
+        saludoClaseBt.setOnClickListener{
+            Toast.makeText(this, "Hola clase", Toast.LENGTH_LONG).show()
+        }
+
+        /*saludoBt.setOnClickListener(this)
+        noSaludoBt.setOnClickListener(this)
+        saludoClaseBt.setOnClickListener(this)*/
     }
 
-    private fun initEvent(){
-        boton.setOnClickListener{
-            Toast.makeText(this, "Hola, funcioné", Toast.LENGTH_LONG).show()
-            texto.text = "Hola, funcioné"
+    /*override fun onClick(botonPulsado: View){
+        when(botonPulsado.id){
+            saludoBt.id -> Toast.makeText(this, "Hola usuario", Toast.LENGTH_LONG).show()
+
+            noSaludoBt.id -> Toast.makeText(this, "No te saludo", Toast.LENGTH_LONG).show()
+
+            saludoClaseBt.id -> Toast.makeText(this, "Hola clase", Toast.LENGTH_LONG).show()
         }
     }
+
+    fun capturarEvento(botonPulsado: View){
+        when(botonPulsado.id){
+            saludoBt.id -> Toast.makeText(this, "Hola usuario", Toast.LENGTH_LONG).show()
+
+            noSaludoBt.id -> Toast.makeText(this, "No te saludo", Toast.LENGTH_LONG).show()
+
+            saludoClaseBt.id -> Toast.makeText(this, "Hola clase", Toast.LENGTH_LONG).show()
+        }
+    }*/
+
+
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
