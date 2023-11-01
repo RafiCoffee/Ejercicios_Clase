574bf09fb343444950b6d2828676f4c659a16276
RafiCoffee <rubensanchezdh@hotmail.com>
2023-11-02 00:38:27 +0100

    FrameLayout Ejercicio 1

---
+++ app/src/main/java/com/example/ejercicios_clase/Activity_Secundaria.kt
@@ -0,0 +1,31 @@
+package com.example.ejercicios_clase
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Activity_Secundaria : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secundaria)
+    }
+    override fun onStart() {
+        super.onStart()
+    }
+    override fun onResume() {
+        super.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+    }
+    override fun onRestart() {
+        super.onRestart()
+    }
+    override fun onStop() {
+        super.onStop()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
--- app/src/main/java/com/example/ejercicios_clase/MainActivity.kt
+++ app/src/main/java/com/example/ejercicios_clase/MainActivity.kt
@@ -3,12 +3,29 @@ package com.example.ejercicios_clase
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import android.widget.*
+import android.content.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imagen1 : ImageView
+    private lateinit var imagen2 : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imagen1 = findViewById(R.id.Imagen1)
+        imagen2 = findViewById(R.id.Imagen2)
+
+        imagen1.setOnClickListener(){
+            val intent = Intent(this, Activity_Secundaria :: class.java)
+            startActivity(intent)
+        }
+
+        imagen2.setOnClickListener(){
+            val intent = Intent(this, Activity_Secundaria2 :: class.java)
+            startActivity(intent)
+        }
     }
     override fun onStart() {
         super.onStart()
